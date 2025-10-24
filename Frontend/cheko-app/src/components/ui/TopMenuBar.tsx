f161ce0504cdad96a98cc742a6e36fdd4d2a3a0f
Faisal Altheyab <Faisal.Altheyab@ibm.com>
2025-10-24 16:36:21 +0300

    dark/light bug fix

--- Frontend/cheko-app/src/components/ui/TopMenuBar.tsx
+++ Frontend/cheko-app/src/components/ui/TopMenuBar.tsx
@@ -38,13 +38,13 @@ const TopMenuBarContent: React.FC = () => {
   }, [isMobileMenuOpen, toggleMobileMenu]);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 shadow-md">
+    <nav className="bg-black dark:bg-black shadow-md menu">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link href="/home" className="flex items-center">
-              <span className="text-xl font-bold text-blue-600 dark:text-blue-400">CHEKO</span>
+              <span className="text-xl font-bold text-blue-600 dark:text-blue-400 brand-name">CHEKO</span>
             </Link>
           </div>
           
@@ -57,8 +57,8 @@ const TopMenuBarContent: React.FC = () => {
                   href={item.path}
                   className={`px-3 py-2 rounded-md text-sm font-medium ${
                     isActive(item.path)
-                      ? 'bg-blue-500 text-white'
-                      : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800'
+                      ? 'active-top-menu'
+                      : 'nonactive-top-menu'
                   }`}
                 >
                   {item.name}
@@ -68,36 +68,36 @@ const TopMenuBarContent: React.FC = () => {
           </div>
           
           {/* Theme Toggle Button */}
-          <div className="flex items-center ml-auto mr-2">
-            <label htmlFor="dark-toggle" className="flex items-center cursor-pointer">
-              <div className="relative flex items-center">
-                {/* Sun icon */}
-                <svg className="w-5 h-5 text-yellow-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path>
-                </svg>
-                
-                <input
-                  type="checkbox"
-                  name="dark-mode"
-                  id="dark-toggle"
-                  className="checkbox hidden"
-                  checked={theme === 'dark'}
-                  onChange={toggleTheme}
-                />
-               <label>
-                  {mounted ? <h1>Hello, {theme}!</h1> : null}
-                </label>
-                <div className="relative block border-[1px] dark:border-white border-gray-900 w-14 h-8 rounded-full transition-colors duration-300">
-                  <div className={`absolute left-1 top-1 dark:bg-white bg-gray-800 w-6 h-6 rounded-full shadow-md z-10 transition-all duration-300 ease-in-out transform ${theme === 'dark' ? 'translate-x-6' : ''}`}></div>
+          {mounted ? 
+            <div className="flex items-center ml-auto mr-2">
+              <label htmlFor="dark-toggle" className="flex items-center cursor-pointer">
+                <div className="relative flex items-center dark-light-container">
+                  {/* Sun icon */}
+                  <svg className="text-black dark-light-svg mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path>
+                  </svg>
+                  
+                  <input
+                    type="checkbox"
+                    name="dark-mode"
+                    id="dark-toggle"
+                    className="checkbox hidden"
+                    checked={theme === 'dark'}
+                    onChange={toggleTheme}
+                  />
+                  <div className="relative block bg-black border-[1px] dark:border-white border-gray-900 w-12 h-5 rounded-full transition-colors duration-300 dark-light-pill-container">
+                    <div className={`absolute left-1 top-1 dark:bg-white bg-gray-800 w-4 h-4 dark-light-pill rounded-full shadow-md z-10 transition-all duration-300 ease-in-out transform ${theme === 'dark' ? 'translate-x-6' : ''}`}></div>
+                  </div>
+                  
+                  {/* Moon icon */}
+                  <svg className="text-black dark-light-svg ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                  </svg>
                 </div>
-                
-                {/* Moon icon */}
-                <svg className="w-5 h-5 text-gray-400 dark:text-blue-100 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                </svg>
-              </div>
-            </label>
-          </div>
+              </label>
+            </div>  
+          : null}
+          
           
           {/* Mobile menu button */}
           <div className="md:hidden">
